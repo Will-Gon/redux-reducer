90f37f5bc6ed5361437e774d20d00c03d4b264c3
Will-Gon <will.colocho@gmail.com>
2021-01-12 09:58:55 -0800

    commit tree e41c3b37a2f89d24923e318c9f10779c27478a4e

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            let friendToRemove = state.friends.findIndex(friend => friend.id === action.id)
+            return {friends: [...state.friends.slice(0, friendToRemove), ...state.friends.slice(friendToRemove + 1)]}
+        default: 
+        return state
+    }
 }
